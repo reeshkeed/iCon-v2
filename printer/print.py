42b18c99d9d02926211221c047cce97dbe633f00
reeshkeed <girish.daloso@gmail.com>
2018-09-04 22:28:47 +0800

    Printer files

---
+++ printer/print.py
@@ -0,0 +1,33 @@
+import random, MySQLdb
+from escpos import printer
+from PIL import Image, ImageDraw, ImageFont
+
+#Code printing
+p = printer.File("/dev/usb/lp0")
+
+img = Image.new('RGB', (370, 70), color = (255, 255, 255))
+fnt = ImageFont.truetype('font.ttf', 74)
+
+code = ''.join(random.choice('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ') for i in range(5))
+
+d = ImageDraw.Draw(img)
+d.text((80, 5),code, font=fnt, fill=(0, 0, 0))
+
+img.save('/tmp/code.png')
+
+p.image('logo.png')
+p.image("/tmp/code.png")
+
+p.cut()
+
+#Saving random code to database
+con = MySQLdb.connect("localhost", "admin", "password", "icon")
+
+mycursor = con.cursor()
+
+mycursor.execute("INSERT INTO user(passcode) VALUES('" +code+ "')")
+
+con.commit()
+con.close()
+
+print(mycursor.rowcount, "record inserted.")
